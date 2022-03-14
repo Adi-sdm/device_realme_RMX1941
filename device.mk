2160810c92709cc31f6f1bea1f3cbdd2f2c9ff2d
hijemu <hijemu@Users.noreply.github.com>
2022-03-14 08:14:34 +0700

    RMX1941: Import Media Config as vendor_overlay
    Signed-off-by: AIV-97 <AIV-97@users.noreply.github.com>
    Change-Id: I319edc425b20a772aa90d2970c4651eb7886ac56

--- device.mk
+++ device.mk
@@ -210,6 +210,10 @@ PRODUCT_BOOT_JARS += \
 
 PRODUCT_PACKAGES += \
     ImsServiceBase
+	
+# Vendor Overlay
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor_overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION))
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
