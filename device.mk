6f70989cbfa349eea7c25ea6aee98f037d756920
AIV-97 <AIV-97@users.noreply.github.com>
2022-03-14 08:03:13 +0700

    RMX1941: Build Dirac Package if-exist
    Signed-off-by: AIV-97 <AIV-97@users.noreply.github.com>
    Change-Id: I799e49eb2ab59ca64a63e446b2fba3455e089192

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/realme/RMX1941/RMX1941-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # RealmeDirac
-$(call inherit-product, packages/apps/RealmeDirac/dirac.mk)
+$(call inherit-product-if-exists, packages/apps/RealmeDirac/dirac.mk)
 
 # Parts
 $(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
